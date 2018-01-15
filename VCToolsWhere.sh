a54d139210512c384928b1d8f3c8ffcd75fa294a
Jake Stine <jake.stine@gmail.com>
2018-01-15 09:34:52 -0800

    Support VS2010 toolchain fallback and better integration with cmd/python
    Added CLI switch to return path names formatted as Windows format, which is required when being used outside bash.

--- VCToolsWhere.sh
+++ VCToolsWhere.sh
@@ -9,6 +9,34 @@
 host=x64
 target=x64
 
+CYGPATH_AS_WIN=0
+SHOW_HELP=0
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -w|--win|--windows)
+    CYGPATH_AS_WIN=1
+    shift
+    ;;
+    -u|--unix)
+    CYGPATH_AS_WIN=0
+    shift
+    ;;
+    --help)
+    SHOW_HELP=1
+    shift
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+me=$(basename "$0")
+
 # this utility is based on the following comment which I found in a batch file deep in the
 # belly of the Visual Studio MSBuild / CLI toolchain:
 
@@ -34,6 +62,25 @@ fi
 vswherepath=$(which vswhere 2>/dev/null)
 if [[ -z "$vswherepath" || ! -f "$vswherepath" ]]; then
     vswherepath="$programfilesx86/Microsoft Visual Studio/Installer/vswhere.exe"
+fi
+
+#try to fall back on the legacy VS2010->2013 style, which set up an environment variable like so:
+if [[ ! -f "$vswherepath" ]]; then
+    legacy_tools=${VS100COMNTOOLS}
+    legacy_tools=${legacy_tools:-$VS110COMNTOOLS}
+    legacy_tools=${legacy_tools:-$VS120COMNTOOLS}
+    legacy_tools="$(cygpath "$legacy_tools")../../VC/Bin/amd64"
+    
+    # sometimes uninstallers leave dirs behind which are empty, so test explicitly for a file
+    # we know is in the toolchain: cl.exe
+    if [[ -e "$legacy_tools/cl.exe" ]]; then
+        if [[ "$CYGPATH_AS_WIN" -eq "1" ]]; then
+            echo "$(cygpath -w "$legacy_tools")"
+        else
+            echo "$legacy_tools"
+        fi
+        exit 0
+    fi
 fi
 
 if [[ ! -f "$vswherepath" ]]; then
@@ -63,7 +110,12 @@ if [[ ! -e "$vstoolsversionfile" ]]; then
 fi
 
 vstoolsver=$(cat "$vstoolsversionfile")
-vstoolsbin="$vspath/VC/Tools/MSVC/$toolsdir/bin/Host$host/$target"
+vstoolsbin="$vspath/VC/Tools/MSVC/$vstoolsver/bin/Host$host/$target"
 
-echo $vstoolsbin
+if [[ "$CYGPATH_AS_WIN" -eq "1" ]]; then
+    echo "$(cygpath -w "$vstoolsbin")"
+else
+    echo "$vstoolsbin"
+fi
+
 exit 0
