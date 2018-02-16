3b5a81c948c14f6777e0e055969c6fc0c8c7dbc0
Jake Stine <jake.stine@gmail.com>
2018-02-16 14:14:56 -0800

    added vswhich.sh, a posix-friendly wrapper for vswhere.exe

--- VCToolsWhere.sh
+++ VCToolsWhere.sh
@@ -2,20 +2,40 @@
 #
 # Latest version can probably be found here:
 #    https://github.com/jstine35/VCToolsWhere
+#
+# this utility is based on the following comment which I found in a batch file deep in the
+# belly of the Visual Studio MSBuild / CLI toolchain:
+#
+# @REM The latest/default toolset is read from :
+# @REM    * Auxiliary\Build\Microsoft.VCToolsVersion.default.txt
+# @REM The latest/default redist directory is read from :
+# @REM    * Auxiliary\Build\Microsoft.VCRedistVersion.default.txt
 
-# TODO: auto-detect host 32 bit systems.
-# TODO: make target configurable via CLI.
 
-host=x64
-target=x64
+dir_host=
+dir_target=
+diag_switch=
 
+DIAGNOSTIC=0
+SHOW_TOOLS_VERSION_ONLY=0
 CYGPATH_AS_WIN=0
 SHOW_HELP=0
+CLI_ERROR_ABORT=0
+
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
+    --diagnostic|--diag)
+    DIAGNOSTIC=1
+    diag_switch=--diag
+    shift
+    ;;
+    --toolsver)
+    SHOW_TOOLS_VERSION_ONLY=1
+    shift
+    ;;
     -w|--win|--windows)
     CYGPATH_AS_WIN=1
     shift
@@ -23,6 +43,19 @@ case $key in
     -u|--unix)
     CYGPATH_AS_WIN=0
     shift
+    ;;
+    --target=*)
+    dir_target="${key#*=}"
+    shift
+    ;;
+    --host=*)
+    dir_host="${key#*=}"
+    shift
+    ;;
+    --target|--host)
+    >&2 echo "Switch $key requires a parameter assignment."
+    >&2 echo "Example: $key=x64"
+    CLI_ERROR_ABORT=1
     ;;
     --help)
     SHOW_HELP=1
@@ -35,68 +68,86 @@ case $key in
 esac
 done
 
-me=$(basename "$0")
-
-# this utility is based on the following comment which I found in a batch file deep in the
-# belly of the Visual Studio MSBuild / CLI toolchain:
-
-# @REM The latest/default toolset is read from :
-# @REM    * Auxiliary\Build\Microsoft.VCToolsVersion.default.txt
-# @REM The latest/default redist directory is read from :
-# @REM    * Auxiliary\Build\Microsoft.VCRedistVersion.default.txt
-
-# %ProgramFiles(x86)% from windows environment is not visible to BASH because it
-# has invalid characters in its name.  So we must pull it from cmd.exe in a roundabout way.
-# Check first if there is a $ProgramFilesx86, just in case future Git for Windows provides
-# it with invalid chars stripped in the future.
-
-if [[ -z "$ProgramFilesx86" || ! -e "$programfilesx86" ]]; then
-    programfilesx86=$(cmd //C echo "%ProgramFiles(x86)%" 2>/dev/null)
+if [[ "$CLI_ERROR_ABORT" -eq "1" ]]; then
+    exit 1
 fi
 
-# vswhere.exe comes with Visual Studio Installer as of VS2017 (maybe also 2015).
-# it might also be installed to the path.  Favor the one in the path first, since I prefer
-# favoring situations where someone provides a vswhere.exe override that points to some specific
-# location of their own preference.
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
-vswherepath=$(which vswhere 2>/dev/null)
-if [[ -z "$vswherepath" || ! -f "$vswherepath" ]]; then
-    vswherepath="$programfilesx86/Microsoft Visual Studio/Installer/vswhere.exe"
+me=$(basename "${BASH_SOURCE[0]}")
+mydir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+
+# route diagnostic into stderr so that the stdout result is still valid/parsable by script.
+diagecho() { >&2 echo $@; }
+
+if [[ "$HOSTTYPE" == "x86_64" ]]; then
+    dir_host=${dir_host:-x64}
+    dir_target=${dir_target:-x64}
+else
+    dir_host=${dir_host:-x32}
+    dir_target=${dir_target:-x32}
 fi
 
-#try to fall back on the legacy VS2010->2013 style, which set up an environment variable like so:
-if [[ ! -f "$vswherepath" ]]; then
+result_msw="$("$mydir/vswhich.sh" $diag_switch -- -latest -property installationPath)"
+
+if [[ "$?" -gt "1" ]]; then
+    # vswhich.sh failed due to programmer error or invalid cli options or invalid
+    # instances of vswhere.exe.  All of these are bad mojo.
+    
+    >&2 echo "An internal error occurred while running vswhich.sh, result=$?"
+    >&2 echo "Specify --diag when running this command for more information."
+    exit 1
+fi
+
+if [[ "$?" -eq "1" ]]; then
+    # vswhich.sh failed due to vswhere.exe not found.
+    # So let's look for legacy visual studio...
+    
+    [ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) vswhere.exe not found! Fall back on legacy MSVC 2010->2013 check!"
+    host=$(   [ "$dir_host"   == "x64" ] && echo ""      || echo "x86" )
+    target=$( [ "$dir_target" == "x64" ] && echo "amd64" || echo "x86" )
     legacy_tools=${VS100COMNTOOLS}
     legacy_tools=${legacy_tools:-$VS110COMNTOOLS}
     legacy_tools=${legacy_tools:-$VS120COMNTOOLS}
-    legacy_tools="$(cygpath "$legacy_tools")../../VC/Bin/amd64"
+    legacy_tools="$(cygpath "$legacy_tools")../../VC/Bin/$host$target"
     
     # sometimes uninstallers leave dirs behind which are empty, so test explicitly for a file
     # we know is in the toolchain: cl.exe
     if [[ -e "$legacy_tools/cl.exe" ]]; then
+        if [[ "$SHOW_TOOLS_VERSION_ONLY" -eq "1" ]]; then
+            >&2 echo "Tools version cannot be determined for legacy Visual Studio installs."
+            >&2 echo "Run this command with the --diagnostic switch to enable detailed reporting."
+            exit 1
+        fi
+        
         if [[ "$CYGPATH_AS_WIN" -eq "1" ]]; then
             echo "$(cygpath -w "$legacy_tools")"
         else
             echo "$legacy_tools"
         fi
         exit 0
+    else
+        >&2 echo "ERROR: Unable to discover the location of vswhere.exe"
+        
+        if [[ "$DIAGNOSTIC" -eq "1" ]]; then
+            >&2 echo "This error may mean that you do not have Visual Studio installed, or that your"
+            >&2 echo "installed version is not supported.  You can manually download and install a copy"
+            >&2 echo "of vswhere.exe from either Microsoft/GitHub, or by using Chocolatey package manager:"
+            >&2 echo ""
+            >&2 echo "   $ cinst vswhere"
+            >&2 echo ""
+        else
+            >&2 echo "Run this command with the --diagnostic switch to enable detailed reporting."
+        fi
+        exit 5
     fi
 fi
 
-if [[ ! -f "$vswherepath" ]]; then
-    >&2 echo "ERROR: Unable to discover the location of vswhere.exe"
-    >&2 echo ""
-    >&2 echo "This error may mean that you do not have Visual Studio installed, or that your"
-    >&2 echo "installed version is not supported.  You can manually download and install a copy"
-    >&2 echo "of vswhere.exe from either Microsoft/GitHub, or by using Chocolatey package manager:"
-    >&2 echo ""
-    >&2 echo "   $ cinst vswhere"
-    >&2 echo ""
-    exit 5
-fi
+vspath="$(readlink -f "$result_msw")"
 
-vspath_msw="$("$vswherepath" -property installationPath)"
-vspath="$(readlink -f "$(cygpath "$vspath_msw")")"
+[ "$DIAGNOSTIC" -eq "1" ] && diagecho "vswhere.exe returned: $vspath_msw"
+[ "$DIAGNOSTIC" -eq "1" ] && diagecho "readlink'd as       : $vspath"
 
 vstoolsversionfile="$vspath/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt"
 
@@ -105,12 +156,17 @@ if [[ ! -e "$vstoolsversionfile" ]]; then
     >&2 echo ""
     >&2 echo "Expected to find: $vstoolsversionfile"
     >&2 echo "This error may mean that your Visual Studio doesn't have the VC Toolset"
-    >&2 echo "selected for install somehow."
+    >&2 echo "selected for install."
     exit 5
 fi
 
 vstoolsver=$(cat "$vstoolsversionfile")
-vstoolsbin="$vspath/VC/Tools/MSVC/$vstoolsver/bin/Host$host/$target"
+vstoolsbin="$vspath/VC/Tools/MSVC/$vstoolsver/bin/Host$dir_host/$dir_target"
+
+if [[ "$SHOW_TOOLS_VERSION_ONLY" -eq "1" ]]; then
+    echo $vstoolsver
+    exit 0
+fi
 
 if [[ "$CYGPATH_AS_WIN" -eq "1" ]]; then
     echo "$(cygpath -w "$vstoolsbin")"
---
+++ vswhich.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Main purpose of this script is to encapsulate evaluation of %ProgramFiles(x86)% from an msys environment.
+
+CYGPATH_AS_WIN=0
+RUN_CUSTOM_SWITCH_MODE=0
+
+DIAGNOSTIC=0
+SHOW_HELP=0
+CLI_ERROR_ABORT=0
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]; do
+key="$1"
+case $key in
+    --diagnostic|--diag)
+    DIAGNOSTIC=1
+    shift
+    ;;
+    -w|--win|--windows)
+    CYGPATH_AS_WIN=1
+    shift
+    ;;
+    -u|--unix)
+    CYGPATH_AS_WIN=0
+    shift
+    ;;
+    --help)
+    SHOW_HELP=1
+    shift
+    ;;
+    --)
+    RUN_CUSTOM_SWITCH_MODE=1
+    shift
+    POSITIONAL+=("$@") # save it in an array for later
+    shift $#
+    ;;
+    *)    # unknown option
+    >&2 echo "Unknown option '$key'"
+    >&2 echo "Use -- to pass options into vswhere.exe, ex:"
+    >&2 echo "  $ vswhere.sh -- -property installationPath"
+    #POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+if [[ "$CLI_ERROR_ABORT" -eq "1" ]]; then
+    exit 5
+fi
+
+set -- "${POSITIONAL[@]}" # restore positional parameters
+me=$(basename "$0")
+
+# route diagnostic into stderr so that the stdout result is still valid/parsable by script.
+diagecho() { >&2 echo "$@"; }
+
+# vswhere.exe comes with Visual Studio Installer as of VS2017 v15.2 or later.  Note that it comes
+# with the installer and not any specific Visual Studio product, so updating the installer is enough
+# to acquire it.  Favor that one first, fall back on PATH if it can't be found.
+# Check both PROGRAMFILES and PROGRAMFILES(X86) in case we're actually running on a 32-bit host.
+
+# %ProgramFiles(x86)% from windows environment is not visible to BASH because it
+# has invalid characters in its name.  So we must pull it from cmd.exe in a roundabout way.
+# Check first if there is a $PROGRAMFILESX86, just in case future Git for Windows provides
+# it with invalid chars stripped in the future.
+
+PROGRAMFILESX86=${PROGRAMFILESX86:-$(cmd //C echo "%ProgramFiles(x86)%" 2>/dev/null)}
+
+[ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) PROGRAMFILES    = $PROGRAMFILES"
+[ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) PROGRAMFILESX86 = $PROGRAMFILESX86"
+vswherepath="$PROGRAMFILESX86/Microsoft Visual Studio/Installer/vswhere.exe"
+if [[ ! -f "$vswherepath" ]]; then
+    [ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) vswhere.exe not found in PROGRAMFILESX86, trying PROGRAMFILES"
+    vswherepath="$PROGRAMFILES/Microsoft Visual Studio/Installer/vswhere.exe"
+fi
+
+if [[ ! -f "$vswherepath" ]]; then
+    [ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) vswhere.exe not found in PROGRAMFILES. Looking in PATH..."
+    vswherepath=$(which vswhere) || exit 1
+fi
+
+if [[ "$RUN_CUSTOM_SWITCH_MODE" -eq "1" ]]; then
+    [ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) Custom args: $@"
+    "$vswherepath" "$@" || exit 2
+    exit 0
+fi
+
+vspath_msw="$("$vswherepath" -property installationPath)"
+
+if [[ "$CYGPATH_AS_WIN" -eq "1" ]]; then
+    echo $vspath_msw
+    exit 0
+fi
+
+[ "$DIAGNOSTIC" -eq "1" ] && diagecho "(diag) vswhere.exe returned: $vspath_msw"
+readlink -f "$vspath_msw"
